70c0741d34733d1d826d36da06c263c317b0f4ee
Arrow1205 <hdauvois@gmail.com>
2026-03-02 20:22:00 +0100

    push2

--- page.tsx
+++
@@ -1,9 +0,0 @@
-export default function HomePage() {
-  return (
-    <div className="p-6 pt-12 text-center">
-      <h1 className="text-4xl font-black italic uppercase italic">Bienvenue sur</h1>
-      <h2 className="text-6xl font-black italic uppercase text-[#DFFF00]">CardBulk</h2>
-      <p className="mt-10 text-gray-400">Commence par scanner une carte ou explore ta collection.</p>
-    </div>
-  );
-}
